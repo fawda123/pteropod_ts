181552922400ec17af0666a213a4269d2c0c3097
fawda123 <mbafs2012@gmail.com>
2020-06-10 18:59:24 -0400

    correlation matrix added

--- R/funcs.R
+++ R/funcs.R
@@ -6,6 +6,20 @@ g_legend <- function(a.gplot){
   legend <- tmp$grobs[[leg]]
   return(legend)}
 
+######
+# function for formatting p-values in tables
+p_ast <- function(x){
+  
+  sig_cats <- c('**', '*', 'ns')
+  sig_vals <- c(-Inf, 0.005, 0.05, Inf)
+  
+  out <- cut(x, breaks = sig_vals, labels = sig_cats, right = FALSE)
+  out <- as.character(out)
+  
+  return(out)
+  
+}
+
 # Data to plot for effects
 #
 # modin lm model
